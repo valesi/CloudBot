e43441de6a0f97a368bd667bdd84be070c5c3a76
Shane M <shane@smoore.io>
2017-06-02 23:29:22 -0700

    remind: update for bot theming

--- plugins/remind.py
+++ plugins/remind.py
@@ -23,7 +23,6 @@ from cloudbot import hook
 from cloudbot.util import database
 from cloudbot.util.timeparse import time_parse
 from cloudbot.util.timeformat import format_time, time_since
-from cloudbot.util import colors
 
 
 table = Table(
@@ -106,8 +105,8 @@ def check_reminders(bot, async, db):
             if not conn.ready:
                 return
 
-            remind_text = colors.parse(time_since(added_time, count=2))
-            alert = colors.parse("{}, you have a reminder from $(b){}$(clear) ago!".format(user, remind_text))
+            remind_text = time_since(added_time, count=2)
+            alert = "{}, you have a reminder from $(b){}$(b) ago!".format(user, remind_text)
 
             conn.message(user, alert)
             conn.message(user, '"{}"'.format(message))
@@ -115,9 +114,9 @@ def check_reminders(bot, async, db):
             delta = (remind_time-added_time).seconds
             if delta > (30*60):
                 late_time = time_since(remind_time, count=2)
-                late = "(I'm sorry for delivering this message $(b){}$(clear) late," \
+                late = "(I'm sorry for delivering this message $(b){}$(b) late," \
                        " it seems I was unable to deliver it on time)".format(late_time)
-                conn.message(user, colors.parse(late))
+                conn.message(user, late)
 
             yield from delete_reminder(async, db, network, remind_time, user)
             yield from load_cache(async, db)
@@ -128,7 +127,7 @@ def check_reminders(bot, async, db):
 def remind(text, nick, chan, db, conn, notice, async):
     """<1 minute, 30 seconds>: <do task> -- reminds you to <do task> in <1 minute, 30 seconds>"""
 
-    count = len([x for x in reminder_cache if x[0] == conn.name and x[3] == nick.lower()])
+    count = len([x for x in reminder_cache if x[0] == conn.name and x[1] == nick.lower()])
 
     if text == "clear":
         if count == 0:
@@ -136,7 +135,7 @@ def remind(text, nick, chan, db, conn, notice, async):
 
         yield from delete_all(async, db, conn.name, nick)
         yield from load_cache(async, db)
-        return "Deleted all ({}) reminders for {}!".format(count, nick)
+        return "Deleted all ({}) reminders!".format(count)
 
     # split the input on the first ":"
     parts = text.split(":", 1)
@@ -151,7 +150,7 @@ def remind(text, nick, chan, db, conn, notice, async):
                "clear your reminders to add any more."
 
     time_string = parts[0].strip()
-    message = colors.strip_all(parts[1].strip())
+    message = parts[1].strip()
 
     # get the current time in both DateTime and Unix Epoch
     current_epoch = time.time()
@@ -160,7 +159,7 @@ def remind(text, nick, chan, db, conn, notice, async):
     # parse the time input, return error if invalid
     seconds = time_parse(time_string)
     if not seconds:
-        return "Invalid input."
+        return "Invalid timespan format."
 
     if seconds > 2764800 or seconds < 60:
         return "Sorry, remind input must be more than a minute, and less than one month."
@@ -175,6 +174,4 @@ def remind(text, nick, chan, db, conn, notice, async):
     yield from load_cache(async, db)
 
     remind_text = format_time(seconds, count=2)
-    output = "Alright, I'll remind you \"{}\" in $(b){}$(clear)!".format(message, remind_text)
-
-    return colors.parse(output)
+    return "Alright, I'll remind you \"{}\" in {}!".format(message, remind_text)
