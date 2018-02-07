177c0fd34bd8b8bc2ca58ab600db9eaa73ab39c9
Shane M <shane@smoore.io>
2018-02-06 16:06:41 -0800

    speedtest: update URLs and parsing for new HTML5 site

--- plugins/speedtest.py
+++ plugins/speedtest.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 import requests
@@ -5,24 +6,28 @@ from lxml import html
 
 from cloudbot import hook
 
-speedtest_re = re.compile(r'.*://(?:\w+)\.speedtest\.net/(?:my-)?result/([0-9]+)?.*', re.I)
-base_url = "http://www.speedtest.net/my-result/{}"
+speedtest_re = re.compile(r'https?://(?:\w+)\.speedtest\.net/(?:my-)?result/(d?/?[0-9]+)', re.I)
+base_url = "http://www.speedtest.net/result/{}"
 
 
 @hook.regex(speedtest_re)
-def speedtest_url(match):
+def speedtest_url(match, bot):
     test_id = match.group(1)
     url = base_url.format(test_id)
 
     request = requests.get(url)
     request.raise_for_status()
-    data = html.fromstring(request.text)
 
-    download = data.xpath('//div[@class="share-speed share-download"]/p')[0].text_content().strip()
-    upload = data.xpath('//div[@class="share-speed share-upload"]/p')[0].text_content().strip()
+    # Disgusting regex hack to get data from JS literal
+    reg = re.search(r'window\.OOKLA\.INIT_DATA={"result":([^}]+})', request.text)
+    data = json.loads(reg.group(1))
 
-    server = data.xpath('//div[@class="share-data share-server"]/p')[0].text_content().strip()
-    ping = data.xpath('//div[@class="share-data share-ping"]/p')[0].text_content().strip()
-    isp = data.xpath('//div[@class="share-data share-isp"]/p')[0].text_content().strip().title()
+    out = []
+    out.append("[h1]DL:[/h1] {0:.2f} Mbps".format(float(data["download"])/1000))
+    out.append("[h1]UL:[/h1] {0:.2f} Mbps".format(float(data["upload"])/1000))
+    out.append("[h1]Ping:[/h1] {}ms".format(data["latency"]))
+    out.append("[h1]ISP:[/h1] {}".format(data["isp_name"]))
+    out.append("[h1]Server:[/h1] {} [h3]({}, {})[/h3]".format(data["sponsor_name"], data["server_name"], data["country_code"]))
 
-    return "[h1]Download:[/h1] {} [div] [h1]Upload:[/h1] {} [div] [h1]Ping:[/h1] {} [div] [h1]ISP:[/h1] {} [div] [h1]Server:[/h1] {}".format(download, upload, ping, isp, server)
+    return " [div] ".join(out)
+
