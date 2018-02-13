bda7808819535ed675ba3a7da7872c1bd5f0392b
Shane M <shane@smoore.io>
2018-02-12 21:28:53 -0800

    rfc: some refactoring, look deeper for date

--- plugins/rfc.py
+++ plugins/rfc.py
@@ -10,34 +10,31 @@ BASE_URL = "https://tools.ietf.org/html/rfc{}"
 
 
 def get_info(rfc_id, show_url=False):
-    # Fetch the first 2001 bytes
     url = BASE_URL.format(rfc_id)
 
     # Only request first 10 KiB
     headers={"Range": "bytes=0-10240"}
     try:
         req = requests.get(url, headers=headers, timeout=5.0, stream=True)
+        req.raise_for_status()
     except Exception as ex:
-        return "Error reaching IETF site: {}".format(ex)
-
-    if not req.ok:
-        return "tools.ietf.org returned HTTP {}".format(req.status_code)
+        return "Error reaching {}: {}".format(req.url, ex)
 
     soup = BeautifulSoup(req.text, "lxml")
-    title = soup.title.text.strip().split(" - ", 1)[1]
     date = "N/A"
-    pre = soup.body.pre
-    # Not \r\n... First 1000 characters
-    for line in pre.text[:1000].split('\n'):
-        print("line: {}".format(line))
+
+    for line in soup.body.pre.text[:2000].split('\n'):
+        # Assuming Date is right-justified and ends with 4 year digits
         if len(line) > 64 and line[-4:].isdigit():
             date = line[-20:].strip()
             break
 
-    out = "{} [div] [h1]Date:[/h1] {}".format(title, date)
+    out = []
+    out.append(soup.title.text.split(" - ", 1)[1].strip())
+    out.append("[h1]Date:[/h1] " + date)
     if show_url:
-        out += " [div] [h3]{}[/h3]".format(url)
-    return out
+        out.append("[h3]{}[/h3]".format(url))
+    return " [div] ".join(out)
 
 
 @hook.regex(rfc_re)
