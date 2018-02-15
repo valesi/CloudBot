8430609659740f42ccd1c79e54c006f6e1a42f29
Shane Moore <shane@smoore.io>
2018-02-15 12:34:51 -0800

    remove dig.py. google_dns: reformat, show more info

--- plugins/google_dns.py
+++ plugins/google_dns.py
@@ -9,49 +9,58 @@ API_URL = "https://dns.google.com/resolve"
 
 
 @hook.command("dig", "dns")
-def google_dns(text):
-    """<host> [type] - Queries Google's DNS-over-HTTPS service for <host> with record type [type] (default A)"""
+def google_dns(text, reply):
+    """<host> [type] - Queries Google's DNS-over-HTTPS for <host> with record [type] (default A. AAAA, MX, SOA, PTR, TXT etc)."""
     args = text.split()
+    raw_host = args.pop(0)
     # Convert to punycode
-    host = args[0].encode("idna").decode("utf-8").lower()
-    qtype = args[1] if len(args) > 1 else "A"
+    host = raw_host.encode("idna").decode("utf-8").lower()
 
-    # Try to autodetect IP for reverse lookups
+    qtype = args[0] if args else "A"
+    # autodetect IP for PTR if rtype wasn't given
+    if not args:
+        try:
+            host = reversename.from_address(host).to_text()
+            qtype = "PTR"
+        except:
+            pass
+
     try:
-        host = reversename.from_address(host).to_text()
-        qtype = "ptr"
-    except:
-        pass
-
-    params = { "name": host, "type": qtype }
-
-    try:
-        req = requests.get(API_URL, params=params, timeout=5.0)
+        req = requests.get(API_URL, params={"name": host, "type": qtype}, timeout=5.0)
+        if not req.ok:
+            return "Error: HTTP {}".format(req.status_code)
         data = req.json()
     except Exception as ex:
-        return "Internal error: {}".format(ex)
+        reply("Internal error: {}".format(ex))
+        raise
 
-    if not req.ok:
-        return "HTTP {}: {}".format(req.status_code, data.get("Comment", "No info"))
+    out = []
 
-    if data["Status"] != 0:
-        info = " [div] [h1]Info:[/h1] " + data["Comment"] if "Comment" in data else ""
-        return "{} [div] {}{}".format(host, rcode.to_text(data["Status"]), info)
-    elif not data.get("Answer"):
-        return "{} [div] No data".format(host)
+    if data["Status"] is not 0:
+        out.append(rcode.to_text(data["Status"]))
+    else:
+        if data.get("Answer"):
+            # Suppress host
+            def format_answer(name, type, data, **kwargs):
+                return "[h1]{}:[/h1] {} [h3]({})[/h3]".format(
+                    # Record type
+                    rdatatype.to_text(type),
+                    # Unescape strings in TXT records etc
+                    data.decode('string_escape') if "\\" in data else data,
+                    # TTL, and any other value
+                    ", ".join([str(kwargs.pop("TTL", ""))] + ["{}: {}".format(k, v) for k, v in kwargs.items()])
+                )
+            # Return first 3 answers, formatted
+            out.extend([format_answer(**ans) for ans in data["Answer"][:3]])
+        else:
+            out.append("No data")
+    if "Comment" in data:
+        out.append(data["Comment"])
 
-    answers = []
-    # Return up to and including first 3 answers
-    for ans in data["Answer"][:3]:
-        rtype = rdatatype.to_text(ans["type"])
-        ttl = ans.get("TTL", "No TTL")
-        answer = ans["data"]
-        # Unescape strings in TXT records etc
-        if "\\" in answer:
-            answer = answer.decode('string_escape')
-        answers.append("{} [h3]({}, {})[/h3]".format(answer, rtype, ttl))
+    if data["AD"]:
+        out.append("[h2]DNSSEC[/h2]")
 
-    answer = " [div] ".join(answers)
+    if raw_host != host:
+        out.append(host)
 
-    return "{} [div] {} [div] [h1]DNSSEC:[/h1] {}".format(host, answer, data["AD"])
-
+    return " [div] ".join(out)
