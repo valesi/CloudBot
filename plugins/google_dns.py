a6dc376959907fb7b762e75ae02c1b6d8c504fbc
Shane Moore <shane@smoore.io>
2018-02-15 17:44:10 -0800

    google_dns: try both A+AAAA and merge

--- plugins/google_dns.py
+++ plugins/google_dns.py
@@ -8,31 +8,68 @@ from cloudbot import hook
 API_URL = "https://dns.google.com/resolve"
 
 
+def query_api(host, qtype, reply):
+    try:
+        req = requests.get(API_URL, params={"name": host, "type": qtype}, timeout=5.0)
+        if not req.ok:
+            reply("Error: HTTP {}".format(req.status_code))
+            return
+        data = req.json()
+    except Exception as ex:
+        reply("Internal error: {}".format(ex))
+        raise
+    else:
+        return data
+
+    return
+
+
 @hook.command("dig", "dns")
 def google_dns(text, reply):
-    """<host> [type] - Queries Google's DNS-over-HTTPS for <host> with record [type] (default A. AAAA, MX, SOA, PTR, TXT etc)."""
+    """<host> [type] - Queries Google's DNS-over-HTTPS for <host> with record [type] (default AAAA and A. MX, SOA, PTR, TXT etc)."""
     args = text.split()
     raw_host = args.pop(0)
     # Convert to punycode
     host = raw_host.encode("idna").decode("utf-8").lower()
 
-    qtype = args[0] if args else "A"
-    # autodetect IP for PTR if rtype wasn't given
-    if not args:
+    a_and_aaaa = False
+    if args:
+        qtype = args[0]
+    else:
         try:
+            # autodetect IP for PTR if rtype wasn't given
             host = reversename.from_address(host).to_text()
             qtype = "PTR"
         except:
-            pass
+            a_and_aaaa = True
+            qtype = "AAAA"
 
-    try:
-        req = requests.get(API_URL, params={"name": host, "type": qtype}, timeout=5.0)
-        if not req.ok:
-            return "Error: HTTP {}".format(req.status_code)
-        data = req.json()
-    except Exception as ex:
-        reply("Internal error: {}".format(ex))
-        raise
+    data = query_api(host, qtype, reply)
+    # API error
+    if not data:
+        return
+
+    data.setdefault("Answer", [])
+
+    # Now check A
+    if a_and_aaaa:
+        ipv4 = query_api(host, "A", reply)
+        if not ipv4:
+            return
+        # Merge into good result
+        if data["Status"] is 0 or ipv4["Status"] is 0:
+            data["Status"] = 0
+        data["Answer"].extend(ipv4.get("Answer", []))
+
+    # Repack and dedupe based on type and data (often CNAME)
+    answers = {}
+    for ans in data["Answer"]:
+        # Suppress hostname
+        del ans["name"]
+        # (rtype, rdata) tuple is answers key. ans is remaining dict data
+        if (ans["type"], ans["data"]) not in answers:
+            answers[ans.pop("type"), ans.pop("data")] = ans
+    data["Answer"] = answers
 
     out = []
 
@@ -40,18 +77,18 @@ def google_dns(text, reply):
         out.append(rcode.to_text(data["Status"]))
     else:
         if data.get("Answer"):
-            # Suppress host
-            def format_answer(name, type, data, **kwargs):
+            def format_answer(key, **kwargs):
+                rtype, rdata = key
                 return "[h1]{}:[/h1] {} [h3]({})[/h3]".format(
                     # Record type
-                    rdatatype.to_text(type),
+                    rdatatype.to_text(rtype),
                     # Unescape strings in TXT records etc
-                    data.decode('string_escape') if "\\" in data else data,
+                    rdata.decode('string_escape') if "\\" in rdata else rdata,
                     # TTL, and any other value
                     ", ".join([str(kwargs.pop("TTL", ""))] + ["{}: {}".format(k, v) for k, v in kwargs.items()])
                 )
-            # Return first 3 answers, formatted
-            out.extend([format_answer(**ans) for ans in data["Answer"][:3]])
+            # Return first 4 answers, formatted
+            out.extend([format_answer(ans, **attribs) for ans, attribs in data["Answer"].items()][:4])
         else:
             out.append("No data")
     if "Comment" in data:
