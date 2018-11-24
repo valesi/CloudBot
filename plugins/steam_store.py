d6b33b33ff04c08ae767819165bfa93936ee5953
Shane Moore <shane@smoore.io>
2018-11-24 10:42:35 -0800

    steam_store: format with bot theme, no short URL

--- plugins/steam_store.py
+++ plugins/steam_store.py
@@ -8,10 +8,10 @@ from cloudbot.util import web, formatting
 
 # CONSTANTS
 
-steam_re = re.compile(r'.*://store.steampowered.com/app/([0-9]+)?.*', re.I)
+steam_re = re.compile('.*://store.steampowered.com/app/([0-9]+)', re.I)
 
-API_URL = "http://store.steampowered.com/api/appdetails/"
-STORE_URL = "http://store.steampowered.com/app/{}/"
+API_URL = "https://store.steampowered.com/api/appdetails/"
+STORE_URL = "https://store.steampowered.com/app/{}/"
 
 
 # OTHER FUNCTIONS
@@ -34,48 +34,36 @@ def format_game(app_id, show_url=True):
     game = data[app_id]["data"]
 
     # basic info
-    out = ["\x02{}\x02".format(game["name"])]
-
-    desc = " ".join(formatting.strip_html(game["about_the_game"]).split())
-    out.append(formatting.truncate(desc, 75))
+    out = ["{} [h3]({})[/h3]".format(game['name'], game['release_date']['date'])]
 
     # genres
     try:
-        genres = ", ".join([g['description'] for g in game["genres"]])
-        out.append("\x02{}\x02".format(genres))
+        genres = ", ".join([g['description'] for g in game['genres']])
+        out.append(genres)
     except KeyError:
         # some things have no genre
         pass
 
-    # release date
-    if game['release_date']['coming_soon']:
-        out.append("coming \x02{}\x02".format(game['release_date']['date']))
-    else:
-        out.append("released \x02{}\x02".format(game['release_date']['date']))
-
     # pricing
     if game['is_free']:
-        out.append("\x02free\x02")
-    elif not game.get("price_overview"):
-        # game has no pricing, it's probably not released yet
-        pass
-    else:
+        out.append("\x02Free\x02")
+    elif game.get("price_overview"):
         price = game['price_overview']
 
-        # the steam API sends prices as an int like "9999" for $19.99, we divmod to get the actual price
-        if price['final'] == price['initial']:
-            out.append("\x02$%d.%02d\x02" % divmod(price['final'], 100))
-        else:
-            price_now = "$%d.%02d" % divmod(price['final'], 100)
-            price_original = "$%d.%02d" % divmod(price['initial'], 100)
+        price_now = "{}\x02{}\x02".format(price['currency'], price['final_formatted'])
 
-            out.append("\x02{}\x02 (was \x02{}\x02)".format(price_now, price_original))
+        if price['final'] == price['initial']:
+            out.append(price_now)
+        else:
+            out.append("{} ($(green)-{}%$(c) from {})".format(price_now, price['discount_percent'], price['initial_formatted']))
+    # else: game has no pricing, it's probably not released yet
+
+    out.append(formatting.truncate(game['short_description'], 250))
 
     if show_url:
-        url = web.try_shorten(STORE_URL.format(game['steam_appid']))
-        out.append(url)
+        out.append("[h3]{}[/h3]".format(STORE_URL.format(game['steam_appid'])))
 
-    return " - ".join(out)
+    return "[h1]Steam:[/h1] " + " [div] ".join(out)
 
 
 # HOOK FUNCTIONS
@@ -83,10 +71,10 @@ def format_game(app_id, show_url=True):
 @hook.command()
 def steam(text, reply):
     """<query> - Search for specified game/trailer/DLC"""
-    params = {'term': text.strip().lower()}
+    params = {'term': text}
 
     try:
-        request = requests.get("http://store.steampowered.com/search/", params=params)
+        request = requests.get("https://store.steampowered.com/search/", params=params)
         request.raise_for_status()
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
         reply("Could not get game info: {}".format(e))
